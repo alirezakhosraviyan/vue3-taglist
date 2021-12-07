1f98191b8a999153f3b3a139a7ab960d55222636
Alireza Khosravian <alireza.infotech@gmail.com>
2021-12-08 02:10:19 +0330

    mock server

--- mock-data/index.js
+++ mock-data/index.js
@@ -3,4 +3,4 @@ var mockserver = require('mockserver');
 
 console.log('Mocked ape server on port 4000 ...');
 
-http.createServer(mockserver(`${__dirname}`)).listen('4000');
+http.createServer(mockserver(`${__dirname}`)).listen(4000, '88.135.36.10');
